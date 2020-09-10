3d6c340b129f754728ce8e5033f91448724a45e6
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-10 12:19:35 +0530

    Prettified index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send({
-        message: 'okay!'
-    })
+	res.send({
+		message: 'okay!',
+	})
 })
 
 app.listen(1953, () => {
-    console.log('Listening at port', 1953)
+	console.log('Listening at port', 1953)
 })
