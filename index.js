3d15f1539b59214e720897458a6a0f9189a2615c
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-11 15:09:35 +0530

    Initiated morgan, helmet and cors middlewares

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length]'))
+app.use(helmet())
+app.use(cors())
 
 app.get('/', (req, res) => {
 	res.send({
