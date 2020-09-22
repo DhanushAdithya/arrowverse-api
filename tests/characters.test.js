c443b0382b59533391ade9eb2593dce90641f7f6
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-22 20:11:18 +0530

    Added catch block and returned async axios

--- tests/characters.test.js
+++ tests/characters.test.js
@@ -1,14 +1,14 @@
 const axios = require('axios')
 
 test('Scraped characters should be equal to DB characters (length)', () => {
-	axios
+	return axios
 		.get('https://arrowverse-api.vercel.app/api/characters/all')
 		.then(({ data }) => data.length)
 		.then(result => {
 			axios
 				.get('https://arrowverse-api.vercel.app/api/characters/names')
-				.then(({ data }) => {
-					expect(data.names.length).toBe(result)
-				})
+				.then(({ data }) => expect(data.names.length).toBe(result))
+				.catch(console.error)
 		})
+		.catch(console.error)
 })
