df8705e81733255a9eaf2da69bc19d9efa7edfee
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-22 13:41:27 +0530

    Added test file for characters count

---
+++ tests/characters.test.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+
+test('Scraped characters should be equal to DB characters (length)', () => {
+	axios
+		.get('https://arrowverse-api.vercel.app/api/characters/all')
+		.then(({ data }) => data.length)
+		.then(result => {
+			axios
+				.get('https://arrowverse-api.vercel.app/api/characters/names')
+				.then(({ data }) => {
+					expect(data.length).toBe(result)
+				})
+		})
+})
