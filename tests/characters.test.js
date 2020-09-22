792da5d47e9df8dc811de7575f0a93324f37e0d9
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-22 13:49:31 +0530

    Fixed test error

--- tests/characters.test.js
+++ tests/characters.test.js
@@ -8,7 +8,7 @@ test('Scraped characters should be equal to DB characters (length)', () => {
 			axios
 				.get('https://arrowverse-api.vercel.app/api/characters/names')
 				.then(({ data }) => {
-					expect(data.length).toBe(result)
+					expect(data.names.length).toBe(result)
 				})
 		})
 })
