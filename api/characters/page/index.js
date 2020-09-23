7149255478459bd534649007f1b9f28467bd66eb
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-23 19:27:57 +0530

    Added pagination route

---
+++ api/characters/page/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const Character = require('../../models/Character')
+
+mongoose.connect(process.env.DB_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+
+module.exports = async (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+	if (req.method === 'OPTIONS') {
+		res.status(200).end()
+		return
+	}
+
+	const { page = 1, limit = 10 } = req.query
+	const result = await Character.find().slice(
+		+page * +limit - +limit,
+		+page * +limit
+	)
+	res.json(result)
+}
