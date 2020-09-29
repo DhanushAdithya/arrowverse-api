d848b4a921f3bc42f38e3de89ec91bf1c3674485
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-29 18:18:53 +0530

    Updated the test file

--- tests/characters.test.js
+++ tests/characters.test.js
@@ -2,11 +2,11 @@ const axios = require('axios')
 
 test('Scraped characters should be equal to DB characters (length)', () => {
 	return axios
-		.get('https://arrowverse-api.vercel.app/api/characters/names')
+		.get('https://arrowverse-api.vercel.app/api/characters?only=names')
 		.then(({ data }) => data.names.length)
 		.then(result => {
 			axios
-				.get('https://arrowverse-api.vercel.app/api/characters/all')
+				.get('https://arrowverse-api.vercel.app/api/characters?all=true')
 				.then(({ data }) => expect(data.length).toBe(result))
 				.catch(console.error)
 		})
