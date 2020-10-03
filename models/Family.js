c3eac08370e8c69b38b1c6568eba33aa7a39dbca
DheekshaGopika <dheekshagopikas@gmail.com>
2020-10-03 12:59:05 +0000

    Added Family model

---
+++ models/Family.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const requiredString = {
+	type: String,
+	required: true,
+}
+
+const familySchema = new mongoose.Schema({
+	name: requiredString,
+	species: String,
+	head: [String],
+	home: mongoose.Schema.Types.Mixed,
+	goal: mongoose.Schema.Types.Mixed,
+	imgUrl: String,
+})
+
+const Family = mongoose.model('family', familySchema)
+
+module.exports = Family
