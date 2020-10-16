2e1de683e1e2cd4084e10352e954d10e28699158
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-10-16 19:01:16 +0530

    Fixed the path of url array for families route

--- api/families/index.js
+++ api/families/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const Family = require('../../models/Family')
 const { fetchNames } = require('../../fetchers')
-const fromUrl = require('../../scrapers/characterScraper')
+const fromUrl = require('../../scrapers/familyScraper')
 
 mongoose.connect(process.env.DB_URL, {
 	useNewUrlParser: true,
