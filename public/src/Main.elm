06bdb79b9d873f3fc108fd9649c2f01acb4e6300
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-10-17 21:23:50 +0530

    Added Elm file for frontend

---
+++ public/src/Main.elm
@@ -0,0 +1,37 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, div, h1, text)
+import Html.Attributes exposing (class)
+
+
+
+-- import Html.Events
+
+
+main : Program () String String
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
+
+
+init : String
+init =
+    "Arrowverse API"
+
+
+update : String -> String -> String
+update msg model =
+    case msg of
+        _ ->
+            model
+
+
+view : String -> Html String
+view model =
+    div []
+        [ h1 [ class "heading" ] [ text model ]
+        ]
